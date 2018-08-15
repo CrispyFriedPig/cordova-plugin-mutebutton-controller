22381c11f8f8f697ba222221aea0ba5228e893bf
Qisheng <qisheng.tian@pactera.com>
2018-08-15 19:15:39 +0800

    读取图片尺寸

--- src/ios/MutebuttonController.m
+++ src/ios/MutebuttonController.m
@@ -15,14 +15,18 @@
 {
     CDVPluginResult* pluginResult = nil;
     NSString* echo = [command.arguments objectAtIndex:0];
-
+    UIImage *image = [UIImage imageWithContentsOfFile:echo];
     if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+        CGFloat width = image.size.width;
+        CGFloat height = image.size.height;
+        NSDictionary *dic = @{@"width":[NSString stringWithFormat:@"%d",width],
+                              @"height":[NSString stringWithFormat:@"%d",height]};
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:dic];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
-
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
 }
 
 @end
