f89afca0a3fde8c9cea72b15087e20b89362260e
Qisheng <qisheng.tian@pactera.com>
2018-08-26 16:19:25 +0800

    回复原版

--- www/MutebuttonController.js
+++ www/MutebuttonController.js
@@ -1,12 +1,4 @@
 var exec = require('cordova/exec');
-
-exports.coolMethod = function (options, success, error) {
-
-    var urlPath = null;
-    var index = null;
-    if (options) {
-        urlPath = options.urlPath;
-        index = options.index;
-    }
-    exec(success, error, 'MutebuttonController', 'coolMethod', [urlPath, index]);
+exports.coolMethod = function (arg0, success, error) {
+  exec(success, error, 'MutebuttonController', 'coolMethod', [arg0]);
 };
